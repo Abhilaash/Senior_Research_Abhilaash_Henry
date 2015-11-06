363e81d8870a316808340885898b9724574f01f5
Abhilaash <Abhilaash.Velamati@gmail.com>
2015-11-06 18:07:45 -0500

    Added OpenCV Files

---
+++ app/src/main/java/edu2016avelamat2016hwang/tjhsst/senior_research_abhilaash_henry/EdgeDetectionActivityFragment.java
@@ -0,0 +1,22 @@
+package edu2016avelamat2016hwang.tjhsst.senior_research_abhilaash_henry;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class EdgeDetectionActivityFragment extends Fragment {
+
+    public EdgeDetectionActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_edge_detection, container, false);
+    }
+}
