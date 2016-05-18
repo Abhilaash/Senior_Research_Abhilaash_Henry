a13c8ab0c8e9bd6682e1cdaba7c123012099d844
hahahhenry <hmw521@gmail.com>
2016-05-18 10:35:40 -0400

    Vibration method

--- app/src/main/java/edu2016avelamat2016hwang/tjhsst/senior_research_abhilaash_henry/HelpActivity.java
+++ app/src/main/java/edu2016avelamat2016hwang/tjhsst/senior_research_abhilaash_henry/HelpActivity.java
@@ -43,45 +43,46 @@ public class HelpActivity extends ActionBarActivity{
             @Override
             public void onClick(View v) {
                 vibrator.cancel();
-                int x = 100;
-                //should be half vibration strength
-                long[] pattern;
-                long length = (long)1000/((long)(250)/x + (long)(25)/x);//
-                //pattern = new long[x * (int)length - 1];
-                pattern = new long[(int)length-1];
-                for(int y = 1; y < x; y+=2)
-                {
-                    pattern[y] = (long) 250 / x;
-                    pattern[y + 1] = (long) 25 / x;
-                }
-                Log.i("Pattern",pattern.toString());
+//                int x = 1000;
+//                long[] pattern;
+//                //long length = (long)1000/( (long)(250)/x + (long)(25)/x);//
+//                pattern = new long[x+1];
+//                //Log.i("Length", ""+length );
+//                //pattern = new long[(int)length-1];
+//                for(int y = 1; y < x; y+=2)
+//                {
+//                    pattern[y] = (long) 2500 / x;
+//                    pattern[y + 1] = (long) 250 / x;
+//                }
+//                //Log.i("Pattern",pattern.toString());
+//                vibrator.vibrate(pattern, -1);
+                long[] pattern = genVibratorPattern((float)0.50, 1000);
                 vibrator.vibrate(pattern, -1);
             }
         });
-
-
-
-
     }
     //Create pattern method
     public long[] createPattern(float intensity, long duration)
     {
         long[] pattern;
-        int x = 100;
-        long length = duration/((long)250/x + (long)25/x);
-        pattern = new long[(int)length-1];
+        int x = (int)duration;
+        //long length = duration/((long)250/x + (long)25/x);
+        pattern = new long[x+1];
         for(int y = 1; y < x; y+= 2)
         {
-            pattern[y] = (long)250/x;
-            pattern[y+1] = (long)25/x;
+            pattern[y] = (long)2500/x;
+            pattern[y+1] = (long)250/x;
         }
         return pattern;
     }
-/*    public long[] genVibratorPattern( float intensity, long duration )
+
+    //http://stackoverflow.com/questions/20808479/algorithm-for-generating-vibration-patterns-ranging-in-intensity-in-android/20821575#20821575
+   public long[] genVibratorPattern( float intensity, long duration )
     {
+        duration = duration/2;
         float dutyCycle = Math.abs( ( intensity * 2.0f ) - 1.0f );//[0,1]
         long hWidth = (long) ( dutyCycle * ( duration - 1 ) ) + 1;
-        long lWidth = dutyCycle == 1.0f ? 0 : 1;
+        long lWidth = (dutyCycle == 1.0f) ? 0 : 1;
 
         int pulseCount = (int) ( 2.0f * ( (float) duration / (float) ( hWidth + lWidth ) ) );
         long[] pattern = new long[ pulseCount ];
@@ -92,5 +93,5 @@ public class HelpActivity extends ActionBarActivity{
         }
 
         return pattern;
-    }*/
+    }
 }
